67978e631f5514a34a9492109e7bcaa64e1859b6
bmrogers2022 <bmrogers@ucsd.edu>
2024-05-07 19:20:59 -0700

    speech synthesis component completed

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,42 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // get all components of web page
+  const selectVoice = document.getElementById('voice-select');
+  const textBox = document.getElementById('text-to-speak');
+  const pressToTalk = document.querySelector('button');
+  const faceImage = document.querySelector('img');
+  const smiling = 'assets/images/smiling.png';
+  const smilingOpen = 'assets/images/smiling-open.png'
+
+  let synth = window.speechSynthesis;
+  let voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+    for(let i = 0; i < voices.length; i++) {
+      let option = document.createElement('option');
+      option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      selectVoice.appendChild(option);
+    }
+  }
+
+  populateVoiceList();
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+
+  pressToTalk.addEventListener('click', () => {
+    let textToSpeak = textBox.value;
+    let utterance = new SpeechSynthesisUtterance(textToSpeak);
+    let selectedVoiceName = selectVoice.selectedOptions[0].getAttribute('data-name');
+    utterance.voice = voices.find(voice => voice.name === selectedVoiceName);
+    utterance.onstart = () => {
+      faceImage.src = smilingOpen;
+    };
+    utterance.onend = () => {
+      faceImage.src = smiling;
+    };
+    synth.speak(utterance);
+  });
 }
